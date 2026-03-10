33a9092e60e5df12a228f71c04cd7a357b05edc1
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2026-03-10 12:54:24 +0200

    add command to release notes

--- releasenotes.sh
+++ releasenotes.sh
@@ -4,4 +4,10 @@ set -eu -o pipefail
 
 wget -k https://portswigger.net/burp/releases/professional-community-edition-$(tr '.' '-' <<< "$VERSION") -O release.html
 pup 'div#content' < release.html | pandoc --from=html --to=gfm --wrap=none -o release.md
+cat >> release.md<< EOF
+run this version
+\`\`\`sh
+nix run 'github:yechielw/burpsuite.nix?ref=$VERSION'
+\`\`\`
+EOF
 
