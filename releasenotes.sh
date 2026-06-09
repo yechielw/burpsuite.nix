2352f99e8616129bac5d4ab5247c3e8d58f0fd46
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2026-06-09 14:51:59 +0300

    fix release notes html tag

--- releasenotes.sh
+++ releasenotes.sh
@@ -15,8 +15,9 @@ case "$release_path" in
 esac
 
 wget -k "$release_url" -O release.html
-pup 'div#content' < release.html | pandoc --from=html --to=gfm --wrap=none -o release.md
-cat >> release.md<< EOF
+pup 'div#content' < release.html | pandoc --from=html --to=gfm-raw_html --wrap=none -o release.md
+cat >> release.md << EOF
+
 run this version
 \`\`\`sh
 nix run 'github:yechielw/burpsuite.nix?ref=$VERSION'
